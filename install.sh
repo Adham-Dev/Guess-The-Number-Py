379b9c4cc3b9f73874124a00d19c868ab3a95d14
Adham <iamnotadham@gmail.com>
2024-04-30 13:57:30 +0300

    install and readme

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir ~/.pyfile
+
+cp guess_num.py ~/.profile
+
+alais guess="python3 ~/.profile/guess_num.py"
---
+++ readme.md
@@ -0,0 +1,4 @@
+This game made by "Adham"
+make sure to install python3
+To lunch game write guess in terminal
+
